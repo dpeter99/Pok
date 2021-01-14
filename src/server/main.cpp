168e8397c55676749c25bcd2960a4313f34472d9
Curle <curle@gemwire.uk>
2021-01-14 23:58:10 +0000

    Un-obliterate working tree, add sample database with testing and Erythro binaries

--- src/server/main.cpp
+++ src/server/main.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /****************************
  * Pok Package Manager
  * Server Endpoint
@@ -11,7 +10,7 @@
  */
 
 #define OPENSSL_API_1_1
-#include <inc/CivetServer.h>
+#include <inc/civetserver.hpp>
 #include <inc/json.h>
 using json = nlohmann::json;
 
@@ -20,6 +19,7 @@ using json = nlohmann::json;
 #include <fcntl.h>
 #include <stdio.h>
 #include <iostream>
+#include <csignal>
 #include <fstream>
 #include <cmath>
 
@@ -29,6 +29,17 @@ using json = nlohmann::json;
     #include <unistd.h>
 #endif
 
+bool enabled = true;
+
+bool shouldRun() {
+    return enabled;
+}
+
+void shutdown(int sig) {
+    printf(" \x1b[32m✔ Shutting down \x1b[0m\n");
+    mg_exit_library();
+    enabled = false;
+}
 
 class ResponseHelper {
     private: 
@@ -57,7 +68,7 @@ class ResponseHelper {
 
         void sendHeader(mg_connection* connect) {
             if (status_code == 127001 || content_type == "dead") {
-                wprintf(L"   \x1b[33m✖ Invalid state! Construct the response first.");
+                printf("   \x1b[33m✖ Invalid state! Construct the response first.\r\n");
             }
 
             std::string response = "HTTP/1.1 ";
@@ -79,44 +90,28 @@ class ResponseHelper {
 
 class PokHandler : public CivetHandler {
     private:
-        const std::wstring searchPrefix = L"/search/";
-        const std::wstring versionsPostfix = L"/versions";
-        const std::wstring archivePostfix = L"/archive";
+        const std::string searchPrefix = "/search/";
+        const std::string versionsPostfix = "/versions";
+        const std::string archivePostfix = "/archive";            
+        //Load the meta json
+        std::ifstream metaFile;
+
         ResponseHelper header;
 
-        static std::wstring GetURI(const struct mg_request_info* request_info) {
+        static std::string GetURI(const struct mg_request_info* request_info) {
             const char* uri = request_info->request_uri;
-            std::wstring s(charArrToWstring(uri));
+            std::string s(uri);
             return s;
         };
 
-        static std::wstring GetConnectionSource(const struct mg_request_info* request_info) {
+        static std::string GetConnectionSource(const struct mg_request_info* request_info) {
             const char* addr = request_info->remote_addr;
-            std::wstring s(charArrToWstring(addr));
+            std::string s(addr);
             return s;
         };
 
-        static std::wstring charArrToWstring(const char* in) {
-            std::string s(in);
-            std::wstring wide(s.begin(), s.end());
-            return wide;
-        }
-
-        static const char* wstringToCharArr(std::wstring in) {
-            std::string temp(in.begin(), in.end());
-            return temp.c_str();
-        }
-
-        static const std::wstring stringToWstring(std::string in) {
-            return std::wstring(in.begin(), in.end());
-        }
-
-        static const std::string wstringToString(std::wstring in) {
-            return std::string(in.begin(), in.end());
-        }
-
-        static std::vector<std::wstring> listDir(const char* path) {
-            std::vector<std::wstring> entries;
+        static std::vector<std::string> listDir(const char* path) {
+            std::vector<std::string> entries;
 
             struct dirent *entry;
             DIR *dir = opendir(path);
@@ -125,7 +120,7 @@ class PokHandler : public CivetHandler {
             }
 
             while ((entry = readdir(dir)) != NULL) {
-                entries.emplace_back(charArrToWstring(entry->d_name));
+                entries.emplace_back(entry->d_name);
             }
 
             closedir(dir);
@@ -135,17 +130,24 @@ class PokHandler : public CivetHandler {
 
     public:
         PokHandler() {
+            metaFile.open("./packages/meta.json");
+            if(!metaFile) {
+                std::cerr << "Unable to open package data!" << std::endl;
+                shutdown(2);
+            }
         }
+
+
 
         bool handleGet(CivetServer* server, struct mg_connection* connection) {
             const struct mg_request_info* req = mg_get_request_info(connection);
-            const std::wstring url = charArrToWstring(req->request_uri);
-            std::wcout << L" \x1b[36m➡ Incoming request from " << PokHandler::GetConnectionSource(req) << " for path " << PokHandler::GetURI(req) << std::endl;
+            const std::string url = req->request_uri;
+            std::cout << " \x1b[36m=> Incoming request from " << PokHandler::GetConnectionSource(req) << " for path " << PokHandler::GetURI(req) << std::endl;
             
             //helper.setStatusCode(200).setContentType("text/plain");
             //std::string resStr("You have reached the Gemwire Pok Thing That I Made For The Purpose Of Existing");
 
-            std::wstring queryResult(
+            std::string queryResult(
                 url.find(searchPrefix) == 0 ? searchFor(url.substr(8)) // startsWith("/search/");
                 : url.rfind(versionsPostfix) == (url.size() - versionsPostfix.size()) ? fetchVersions(url.substr(1, url.size() - 9)) // endsWith("/versions");
                 : retrieveMeta(url.substr(1), true)); // Default to just printing information
@@ -153,123 +155,121 @@ class PokHandler : public CivetHandler {
             // We can't printf a file, so we have to special-case for the archive
             if(url.rfind(archivePostfix) == (url.size() - archivePostfix.size())) {
                 // We need to fetch the archive and set the header info before we send the header
-                std::wstring target(fetchArchive(url.substr(1, url.size() - 8))); // endsWith("/archive");)
+                std::string target(fetchArchive(url.substr(1, url.size() - 8))); // endsWith("/archive");)
                 header.sendHeader(connection);
-                if(!target.compare(L"")) {
+                if(!target.compare("")) {
                     // file_body takes a CString representing the file path
-                    mg_send_file_body(connection, wstringToCharArr(target));
+                    mg_send_file_body(connection, target.c_str());
                 }
             } else {
                 // If we're not looking for an archive, send the header and printf the content
                 header.sendHeader(connection);
-                mg_printf(connection, wstringToCharArr(queryResult));
+                mg_printf(connection, queryResult.c_str());
 
             }
 
             return true;
         };      
 
-        std::wstring retrieveMeta(std::wstring item, bool send) {
-            std::wcout << L"   \x1b[36m➡ Searching for " << item << " in package repository" << std::endl;
-            
-            //Load the meta json
-            std::ifstream metaFile("packages/meta.json");
+        std::string retrieveMeta(std::string item, bool send) {
+
+            if(!metaFile)
+                return R"(
+{
+    "error": 2,
+    "meaning": "Package Meta Corrupt",
+    "versions": "N/A"
+}
+                )";
+
+            std::cout << "   \x1b[36m=> Searching for " << item << " in package repository" << std::endl;
+
             json packages;
             metaFile >> packages;
-
             // Find the item
             for(auto& temp : packages) {
                 for(auto& element : temp.items()) {
                     auto val = element.value();
-                    if(stringToWstring(val["name"].get<std::string>()) == item) {
-                        std::wcout << L"     \x1b[36m➡ Found element " << stringToWstring(val["name"].get<std::string>()) << std::endl;
+                    if(val["name"].get<std::string>() == item) {
+                        std::cout << "     \x1b[36m=> Found element " << val["name"].get<std::string>() << std::endl;
                         if(send) header.setStatusCode(200).setContentType("application/json");
-                        return stringToWstring(element.value().dump());
+                        return element.value().dump();
                
                     }
                 }
             }
 
-            std::wcout << L"     \x1b[36m➡ Package not found." << std::endl;
+            std::cout << "     \x1b[36m=> Package not found." << std::endl;
             header.setStatusCode(404).setContentType("application/json");
 
-            return std::wstring(LR"(
+            return R"(
                 {
                     "error": 1,
                     "meaning": "Package Not Found",
                     "versions": "N/A"
                 }
-            )");
+            )";
 
 
         }
 
-        std::wstring searchFor(std::wstring item) {
+        std::string searchFor(std::string item) {
             return retrieveMeta(item, true);
         };
 
-        std::wstring fetchVersions(std::wstring item) {
+        std::string fetchVersions(std::string item) {
             return retrieveMeta(item, true);
         };
 
-        std::wstring fetchArchive(std::wstring item) {
+        std::string fetchArchive(std::string item) {
             const auto package = item.substr(0, item.find_first_of('/'));
             auto version = item.substr(item.find_first_of('/') + 1);
             // Retrieve package meta for versioning
             json meta = json::parse(retrieveMeta(package, false));
             // Short-circuit on error
-            if(meta.contains("error")) return L"";
+            if(meta.contains("error")) return "";
             // If latest or none, get the highest version
-            if(version.compare(L"latest/") || version.compare(L"")) {
+            if(version.compare("latest/") || version.compare("")) {
                 auto metaVersions = meta["versions"];
-                std::string strDump(metaVersions[metaVersions.size() - 1].dump());
-                std::wstring wideDump(strDump.begin(), strDump.end());
-                version = wideDump;
+                version = metaVersions[metaVersions.size() - 1].dump();
             }
             
             // List the files in /packages/package/version/
-            std::wstring fileToRead;
-            std::wstring targetFolder = L"/packages/";
-            targetFolder.append(package).append(L"/").append(version);
-            std::vector<std::wstring> names(listDir(wstringToCharArr(targetFolder)));
+            std::string fileToRead;
+            std::string targetFolder = "/packages/";
+            targetFolder.append(package).append("/").append(version);
+            std::vector<std::string> names(listDir(targetFolder.c_str()));
             if(names.size() > 1) {} // TODO: zip?
 
-            fileToRead = targetFolder.append(L"/").append(names.at(0));
-            std::wcout << L"   \x1b[36m➡ Serving " << fileToRead << std::endl;
+            fileToRead = targetFolder.append("/").append(names.at(0));
+            std::cout << "   \x1b[36m=> Serving " << fileToRead << std::endl;
             header.setStatusCode(200).setContentType("application/octet-stream");
             return fileToRead;
         };
 };
 
 int main(void) {
-    _setmode(_fileno(stdout), _O_U16TEXT);
+
+
     mg_init_library(0);
 
     std::vector<std::string> opts = { "document_root", "/web", "listening_ports", "15060" };
 
     CivetServer server(opts);
 
-    wprintf(L"---\n\n\x1b[7m.:Gemwire Pok Server:.\x1b[0m\n");
-    wprintf(L" \x1b[36m➡ Starting up..\n");
+    std::cout << "---\n\n\x1b[7m.:Gemwire Pok Server:.\x1b[0m\n" << std::endl;
+    std::cout << " \x1b[36m=> Starting up..\n" << std::endl;
 
     PokHandler handler_reqs;
     server.addHandler("", handler_reqs); // Redirect /** to handler_reqs
 
-    while(
-    #ifdef _WIN32
-        GetAsyncKeyState(VK_ESCAPE)==0
-    #else    
-        0
-    #endif
-        ) {
-        Sleep(1000);
-    }
+    signal(SIGINT, shutdown);
+    
 
-    wprintf(L" \x1b[32m✔ Shutting down \x1b[0m\n");
-    mg_exit_library();
+    while(shouldRun()) {
+        Sleep(1000);
+    };
 
     return 0;
 
 }
-=======
->>>>>>> parent of 68b315c... Client & Server, now talking to each other.
